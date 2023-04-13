fdbd576f2d4349dfcf8d289f96133a029f57845b
Gene Kogan <kogan.gene@gmail.com>
2023-04-12 21:49:34 -0700

    make metadata optional

--- examples/upload_media.js
+++ examples/upload_media.js
@@ -2,5 +2,7 @@ import {EdenClient} from "eden-sdk";
 
 const eden = new EdenClient();
 
-let result = await eden.uploadFile("/Users/genekogan/Downloads/monster2.wav");
+//let result = await eden.uploadFile("/Users/genekogan/Downloads/monster2.wav");
+console.log("lets do it!")
+let result = await eden.uploadFile("/Users/genekogan/Downloads/c0b2dfbf24607bb7ca73c237116f0143 copysss.png");
 console.log(result);
--- index.d.ts
+++ index.d.ts
@@ -76,10 +76,10 @@ declare class EdenClient {
   
   getCreations(filter: any | null): Promise<Creation[]>;
   getCreation(creationId: string): Promise<Creation>;
-  startTask(generatorName: string, config: any, metadata: any, generatorVersion?: string): Promise<any>;
+  startTask(generatorName: string, config: any, metadata?: any, generatorVersion?: string): Promise<any>;
   getTasks(filter: any | null): Promise<any>;
   getTaskStatus(taskId: string): Promise<any>;
-  create(generatorName: string, config: any, metadata: any, generatorVersion?: string): Promise<any>;
+  create(generatorName: string, config: any, metadata?: any, generatorVersion?: string): Promise<any>;
 
   getCollections(userId: string | null): Promise<Collection[]>;
   getCollection(name: any): Promise<Collection>;
