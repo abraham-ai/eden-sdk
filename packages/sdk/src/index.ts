58e60bb9a8d73045cd2f671b694927031f00db74
Jonathan Miller <jon@jmill.dev>
2023-05-24 20:40:18 -0700

    add index

---
+++ packages/sdk/src/index.ts
@@ -0,0 +1,9 @@
+export {
+  EdenClient,
+  WebClientOptions,
+  WebAPICallOptions,
+  WebAPICallResult,
+} from './EdenClient';
+
+export * from './methods';
+export { default as Method } from './methods';
